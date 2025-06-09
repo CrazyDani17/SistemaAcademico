e2b9fd8520049e54c9fefb65e33ebe516d0b9d1c
daniel mendiguri <daniel.mendiguri@cultural.edu.pe>
2025-06-09 10:02:23 -0500

    fixed errors

--- config/backpack/base.php
+++ config/backpack/base.php
@@ -73,7 +73,7 @@ return [
     // ------
 
     // Menu logo. You can replace this with an <img> tag if you have a logo.
-    'project_logo'   => "<img src='/imgs/ubdevs_logo.png' alt='Logo' width='128' height='65'>",
+    'project_logo'   => "<img src='/imgs/logoejemplo.png' alt='Logo' width='128' height='65'>",
 
     // Show / hide breadcrumbs on admin panel pages.
     'breadcrumbs' => true,
@@ -107,7 +107,7 @@ return [
     // change background color with bg-dark, bg-primary, bg-secondary, bg-danger, bg-warning, bg-success, bg-info, bg-blue, bg-light-blue, bg-indigo, bg-purple, bg-pink, bg-red, bg-orange, bg-yellow, bg-green, bg-teal, bg-cyan, bg-white
 
     // Developer or company name. Shown in footer.
-    'developer_name' => 'UBDevs',
+    'developer_name' => '',
 
     // Developer website. Link in footer. Type false if you want to hide it.
     //'developer_link' => 'http://tabacitu.ro',
